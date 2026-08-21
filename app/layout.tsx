1708c292a600bff8afd45ca1ae70f45240957d60
websiteloginpril-hub <websitelogin.pril@gmail.com>
2026-08-21 10:11:18 +0530

    removing the variables of chatbase

--- app/layout.tsx
+++ app/layout.tsx
@@ -92,31 +92,8 @@ export default function RootLayout({
             <Toaster />
           </LenisProvider>
         </GoogleAuthProvider>
-        {/*
-        <Script
-          id="chatbase-script"
-          strategy="lazyOnload"
-          dangerouslySetInnerHTML={{
-            __html: `
-              if(!window.chatbase||window.chatbase("getState")!=="initialized"){
-                window.chatbase=(...args)=>{if(!window.chatbase.q){window.chatbase.q=[]}window.chatbase.q.push(args)};
-                window.chatbase=new Proxy(window.chatbase,{
-                  get(target,prop){if(prop==="q"){return target.q}return(...args)=>target(prop,...args)}
-                });
-              }
-              const onLoad=function(){
-                if(document.getElementById("d8bJDvq40KN0i1VPL9vY7")) return;
-                const script=document.createElement("script");
-                script.src="https://www.chatbase.co/embed.min.js";
-                script.id="d8bJDvq40KN0i1VPL9vY7";
-                script.domain="www.chatbase.co";
-                document.body.appendChild(script);
-              };
-              if(document.readyState==="complete"){onLoad();}else{window.addEventListener("load",onLoad);}
-            `,
-          }}
-        />
-        */}
+
+
       </body>
     </html>
   );
